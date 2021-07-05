131af898950a4467c0d4c52fe3e1e83f17966958
Rafael Eiki <rafaeleikimi@gmail.com>
2021-07-05 01:26:56 -0300

    Created carousel with computing definition phrases

--- src/containers/landing-page/LandingSection.js
+++ src/containers/landing-page/LandingSection.js
@@ -1,19 +1,49 @@
-function LandingSection() {
-  return (
-    <section className="hero is-info is-fullheight">
-        <div className="hero-head">
-        </div>
+import { useEffect } from 'react';
 
-        <div className="hero-body">
-            <div className="container has-text-centered">
-            <p className="title">
-                Instituto de Computação da Unicamp
-            </p>
-            <p className="subtitle">
-                Unicamp de Portas Abertas 2021
-            </p>
-            </div>
-        </div>
+import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
+
+import './LandingSection.css';
+
+import computingDefinitions from '../../data/computing-definitions.json';
+
+const { phrases } = computingDefinitions;
+
+function LandingSection() {
+
+  const generateBulma = () => {
+    bulmaCarousel.attach('.hero-carousel', {
+      autoplay: true,
+      loop: true,
+      pauseOnHover: false,
+      autoplaySpeed: 6000,
+      slidesToScroll: 1,
+      slidesToShow: 1
+    });
+  };
+
+  useEffect(() => {
+    generateBulma();
+  });
+
+  const carouselItems = phrases.map((singlePhrase, index) => (
+    <div key={index} className={`item-${index + 1} p-2 pt-6 has-text-centered slide`}>
+      <p className="subtitle slide-text">“{singlePhrase}”</p>
+    </div>
+  ));
+
+  return (
+    <section className="hero is-dark is-fullheight has-carousel">
+
+      <div className="hero-carousel">{carouselItems}</div>
+      <div className="hero-head pt-6">
+        <div className="container has-text-centered p-2">
+          <p className="title">
+            Instituto de Computação
+          </p>
+          <p className="subtitle">Unicamp de Portas Abertas 2021</p>
+          <p className="subtitle">20 a 23 de julho</p>
+        </div> 
+      </div>
     </section>
   );
 }
