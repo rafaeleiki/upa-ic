dd06ad9ab7bddcb33f375a72fa7ecb75e5cfb719
Rafael Eiki <rafaeleikimi@gmail.com>
2021-07-04 23:58:40 -0300

    Using tab size 2

--- src/containers/landing-page/ScheduleSection.js
+++ src/containers/landing-page/ScheduleSection.js
@@ -3,29 +3,29 @@ import schedule from '../../data/schedule.json';
 const { columns, columnsNames, data } = schedule;
 
 function ScheduleItem(props) {
-  const {item} = props;
+  const { item } = props;
 
   return (<tr>
-    { columns.map((column) => (<td>{ item[column] }</td>)) }
+    {columns.map((column) => (<td>{item[column]}</td>))}
   </tr>);
 }
 
 function Schedule() {
-  const tableHeaders = columns.map((column) => (<th>{ columnsNames[column] }</th>));
+  const tableHeaders = columns.map((column) => (<th>{columnsNames[column]}</th>));
 
   return (
     <section className="section">
-        <h2 className="title">Programação</h2>
+      <h2 className="title">Programação</h2>
 
-        <div className="container is-widescreen">
-          <table className="table is-striped is-centered">
-            <thead>{ tableHeaders }</thead>
-            <tfoot>{ tableHeaders }</tfoot>
-            <tbody>
-              { data.map((scheduleItem) => <ScheduleItem item={scheduleItem} />) }
-            </tbody>
-          </table>
-        </div>
+      <div className="container is-widescreen">
+        <table className="table is-striped is-centered">
+          <thead>{tableHeaders}</thead>
+          <tfoot>{tableHeaders}</tfoot>
+          <tbody>
+            {data.map((scheduleItem) => <ScheduleItem item={scheduleItem} />)}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 }
