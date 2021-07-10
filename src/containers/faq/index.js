3ec277906fd289e47fcd856f0b8edfb0c8b1f309
Rafael Eiki <rafaeleikimi@gmail.com>
2021-07-09 23:02:10 -0300

    Created structure for FAQ

--- src/containers/faq/index.js
+++ src/containers/faq/index.js
@@ -1,7 +1,23 @@
+import faqData from "../../data/faq.json";
+
+const { data } = faqData;
+
+const renderQuestion = ({ question, answer }) => (
+  <div className="container is-max-desktop mb-4">
+    <details className="box has-text-left">
+      <summary className="has-text-centered is-size-5 has-text-weight-bold">{question}</summary>
+      {answer}
+    </details>
+  </div>
+);
+
 function FAQPage() {
   return (<section class="section">
     <h1 class="title">FAQ</h1>
     <h2 class="subtitle">Perguntas frequentes sobre os nossos cursos</h2>
+
+    { data.map(renderQuestion) }
+    
   </section>);
 }
 
