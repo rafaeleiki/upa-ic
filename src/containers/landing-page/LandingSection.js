c078446a77a0339ab7068261c8e43ec6ceb1cb74
rafael.eiki <rafael.eiki@quintoandar.com.br>
2021-07-13 22:58:01 -0300

    Fixed carousel layout when window is resized

--- src/containers/landing-page/LandingSection.js
+++ src/containers/landing-page/LandingSection.js
@@ -10,20 +10,36 @@ import computingDefinitions from '../../data/computing-definitions.json'
 
 const { phrases } = computingDefinitions
 
+const resetCarouselPagination = (carousel) => {
+  const selectorsList = ['.slider-navigation-previous', '.slider-pagination', '.slider-navigation-next'];
+  selectorsList.forEach((selector) => {
+    const nodesList = document.querySelectorAll(selector);
+    nodesList.forEach((node) => {
+      node.parentNode.removeChild(node);
+    });
+  });
+
+  carousel.reset();
+}
+
 function LandingSection() {
   const generateBulma = () => {
-    bulmaCarousel.attach('.hero-carousel', {
+    return bulmaCarousel.attach('.hero-carousel', {
       autoplay: true,
       loop: true,
       pauseOnHover: false,
       autoplaySpeed: 6000,
       slidesToScroll: 1,
       slidesToShow: 1,
-    })
+    });
   }
 
   useEffect(() => {
-    generateBulma()
+    const [carousel] = generateBulma()
+
+    window.addEventListener('resize', () => {
+      resetCarouselPagination(carousel);
+    })
   })
 
   const carouselItems = phrases.map((singlePhrase, index) => (
@@ -47,6 +63,8 @@ function LandingSection() {
           <p className="subtitle">20 a 23 de julho</p>
         </div>
       </div>
+      <div class="hero-body"></div>
+			<div class="hero-foot"></div>
     </section>
   )
 }
