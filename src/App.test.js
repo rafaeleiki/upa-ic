668bad44d579d34493f017b573d107b10bde9a74
rafael.eiki <rafael.eiki@quintoandar.com.br>
2021-07-14 00:40:47 -0300

    Added social media icons

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
 import App from './App'
 
 import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js'
@@ -6,8 +8,6 @@ jest.mock('bulma-carousel/dist/js/bulma-carousel.min.js', () => ({
   attach: jest.fn(),
 }))
 
-test('renders title', () => {
-  render(<App />)
-  const title = screen.getByText(/Instituto de Computação/i)
-  expect(title).toBeInTheDocument()
+test('renders app', () => {
+  render(<App />, { wrapper: MemoryRouter })
 })
